976f8914c8e85358a0d7fd4311f37940862fedbc
Riley John Gibbs <rileyjohngibbs@gmail.com>
2023-10-23 11:54:18 -0700

    Styling changes
    Make whole div clickable for next game arrow.
    Make choices boxes square and uniform.

--- src/SequenceFinisher.js
+++ src/SequenceFinisher.js
@@ -64,13 +64,9 @@ const SequenceFinisher = ({ sequence, index, wrongs, endCallback }) => {
 };
 
 const NextArrow = ({ clickHandler }) => (
-  <div className="next-arrow-container">
+  <div className="next-arrow-container" onClick={() => clickHandler()}>
     <svg className="next-arrow" viewBox="0 0 60 30">
-      <polygon
-        onClick={() => clickHandler()}
-        points="0,10 48,12 45,0 60,15 45,30 48,18 0,20"
-        fill="black"
-      />
+      <polygon points="0,10 48,12 45,0 60,15 45,30 48,18 0,20" fill="black" />
     </svg>
   </div>
 );
