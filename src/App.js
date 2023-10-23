5917cdec50784256c36ab2c3e6dfa21141f6b5bc
Riley John Gibbs <rileyjohngibbs@gmail.com>
2023-10-23 05:01:00 -0700

    First version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+
+import SequenceFinisher from "./SequenceFinisher";
+import sequences from "./sequences.json";
 
 function App() {
+  const [sequenceRound, setSequenceRound] = useState({
+    sequence: [],
+    index: 0,
+    wrongs: [],
+  });
+
+  useEffect(() => {
+    setSequenceRound(generateSequenceRound());
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SequenceFinisher
+        sequence={sequenceRound.sequence}
+        index={sequenceRound.index}
+        wrongs={sequenceRound.wrongs}
+        endCallback={() => setSequenceRound(generateSequenceRound())}
+      />
     </div>
   );
 }
+
+const generateSequenceRound = () => {
+  const source = sequences.upperLetters.sequence;
+  const absIndex = Math.floor(Math.random() * source.length);
+
+  const sequenceOptions = [
+    [absIndex, absIndex + 2],
+    [absIndex - 1, absIndex + 1],
+    [absIndex - 2, absIndex],
+  ].filter(([left, right]) => left >= 0 && right < source.length);
+  console.log([absIndex, sequenceOptions]);
+  const [left, right] =
+    sequenceOptions[Math.floor(Math.random() * sequenceOptions.length)];
+  const sequence = source.slice(left, right + 1);
+
+  const index = absIndex - left;
+  const leftovers = source.filter((_, i) => i < left || i > right);
+  const wrongs = [];
+  while (wrongs.length < 3) {
+    wrongs.push(
+      leftovers.splice(Math.floor(Math.random() * leftovers.length), 1)
+    );
+  }
+  return { sequence, index, wrongs };
+};
 
 export default App;
---
+++ src/SequenceFinisher.js
@@ -0,0 +1,85 @@
+import { useState, useEffect } from "react";
+
+import "./SequenceFinisher.css";
+
+const SequenceFinisher = ({ sequence, index, wrongs, endCallback }) => {
+  const [guessed, setGuessed] = useState(new Set());
+  const [choices, setChoices] = useState([]);
+
+  useEffect(() => {
+    const choices = sequence.filter((_, i) => i === index).concat(wrongs);
+    shuffleArray(choices);
+    setChoices(choices);
+    setGuessed(new Set());
+  }, [sequence, index, wrongs]);
+
+  const guessedCorrect = () => {
+    for (let guess of guessed) {
+      if (choices[guess] === sequence[index]) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  const choicesItemClassName = (itemIndex) => {
+    const root = "choices-item";
+    if (guessed.has(itemIndex)) {
+      const mod = choices[itemIndex] === sequence[index] ? "correct" : "wrong";
+      return `${root} ${root}--${mod}`;
+    }
+    return root;
+  };
+
+  const guessItem = (itemIndex) => {
+    if (guessed.has(itemIndex)) {
+      return;
+    }
+    setGuessed(new Set(guessed.add(itemIndex)));
+  };
+
+  return (
+    <div>
+      <div className="sequence">
+        {sequence.map((item, i) => (
+          <div key={i} className="sequence-item">
+            {i === index && !guessedCorrect() ? "_" : item}
+          </div>
+        ))}
+      </div>
+      <div className="choices">
+        {choices.map((item, i) => (
+          <div
+            key={i}
+            className={choicesItemClassName(i)}
+            onClick={() => guessItem(i)}
+          >
+            {item}
+          </div>
+        ))}
+      </div>
+      {guessedCorrect() ? <NextArrow clickHandler={endCallback} /> : ""}
+    </div>
+  );
+};
+
+const NextArrow = ({ clickHandler }) => (
+  <div className="next-arrow-container">
+    <svg className="next-arrow" viewBox="0 0 60 30">
+      <polygon
+        onClick={() => clickHandler()}
+        points="0,10 48,12 45,0 60,15 45,30 48,18 0,20"
+        fill="black"
+      />
+    </svg>
+  </div>
+);
+
+const shuffleArray = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+};
+
+export default SequenceFinisher;
